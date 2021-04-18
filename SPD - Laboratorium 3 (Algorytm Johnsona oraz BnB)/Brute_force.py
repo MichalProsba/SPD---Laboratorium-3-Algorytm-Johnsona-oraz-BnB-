e69bda87bbd1dc92691c5e3c4590870f883cd692
patrykwieczorek03 <patrykwieczorek03@gmail.com>
2021-04-18 18:28:27 +0200

    ?

---
+++ SPD - Laboratorium 3 (Algorytm Johnsona oraz BnB)/Brute_force.py
@@ -0,0 +1,13 @@
+from Natural_permutation import Natural_permutation
+
+class Brute_force (Natural_permutation) : 
+    def __init__ (self, seed, n, m):
+        super().__init__(seed, n, m)
+        #Początek permutacji
+        self.l = 0
+        #Koniec permutacji
+        self.k = len(self.P) - 1
+
+    def __str__(self):
+        str1 = "===================================================================\n"
+        return str1 + " Pi=" + str(self.Pi) + "\n" + str1 + "\n P=" + str(self.P) + "\n" + str1 + "\n S=" + str(self.S) + "\n" + str1 + "\n C=" + str(self.C) + "\n" + str1 
---
+++ SPD - Laboratorium 3 (Algorytm Johnsona oraz BnB)/Tree.py
@@ -0,0 +1,6 @@
+
+class Node:
+    def __init__ (self, parent, data):
+        self.data = data
+        self.parent = parent
+        self.children = []
